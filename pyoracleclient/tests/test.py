0cfa83af3fd301fc41d71db140a66a5923d69cde
Luca Mingarelli <lucamingarelli@me.com>
2021-12-12 01:10:34 +0100

    Update test.py

--- pyoracleclient/tests/test.py
+++ pyoracleclient/tests/test.py
@@ -2,7 +2,7 @@ import pyoracleclient as pyoc
 
 
 def test_tns():
-  pyoc._delete_all_tns()
+  pyoc.delete_all_tns()
   pyoc.add_tns(name='FSSDB', service_name='servicename.prd.tns',
                protocol1='TCP', host1='host1address', port1=1000,
                protocol2='TCP', host2='host1address', port2=1000)
