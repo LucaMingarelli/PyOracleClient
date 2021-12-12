9fe83d952f3c16bfa2293ec95da6f98f95b94b5a
Luca Mingarelli <lucamingarelli@me.com>
2021-12-12 01:05:45 +0100

    Create test.py

---
+++ pyoracleclient/tests/test.py
@@ -0,0 +1,22 @@
+import pyoracleclient as pyoc
+
+
+def test_tns():
+  pyoc._delete_all_tns()
+  pyoc.add_tns(name='FSSDB', service_name='servicename.prd.tns',
+               protocol1='TCP', host1='host1address', port1=1000,
+               protocol2='TCP', host2='host1address', port2=1000)
+  
+def test_custom_tns():
+  pyoc.add_custom_tns("""yourtnsname = (DESCRIPTION=
+                    (ADDRESS_LIST=(FAILOVER=ON)
+                                  (LOAD_BALANCE = OFF)
+                                  (ADDRESS=(PROTOCOL=TCP)
+                                           (HOST=host1address)
+                                           (PORT=1000))
+                                  (ADDRESS=(PROTOCOL=TCP)
+                                           (HOST=host1address)
+                                           (PORT=1000)))
+                                  (CONNECT_DATA=(SERVER=dedicated)
+                                  (SERVICE_NAME=servicename.prd.tns)))
+                    """)
